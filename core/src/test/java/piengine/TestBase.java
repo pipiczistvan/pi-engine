e10c2cd6585ace689acc224f0507e92151f11ff4
pipiczi <istvan.pipicz@gmail.com>
2017-10-01 20:33:48 +0200

    Modularization

--- core/src/test/java/piengine/TestBase.java
+++ core/src/test/java/piengine/TestBase.java
@@ -5,8 +5,8 @@ import piengine.core.property.domain.ApplicationProperties;
 
 public abstract class TestBase {
 
-    private static final String ENGINE_PROPERTIES = "src/main/resources/config/engine.properties";
-    private static final String APPLICATION_PROPERTIES = "src/test/resources/config/application.properties";
+    private static final String ENGINE_PROPERTIES = "engine";
+    private static final String APPLICATION_PROPERTIES = "application";
 
     @Before
     public void setUp() throws Exception {
